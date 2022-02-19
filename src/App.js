afd60e4f73c682a032eb545db3b457a5f3492eaf
rahathosen <70264343+rahathosen@users.noreply.github.com>
2022-02-19 11:58:22 +0600

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import React from "react";
+
+const LAUNCHES_QUERY = `
+{
+  launchesPast(limit: 10) {
+    id
+    mission_name
+  }
+}
+`;
 function App() {
+  const [launches, setLaunches] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch("https://api.spacex.land/graphql/", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ query: LAUNCHES_QUERY }),
+    })
+      .then((Response) => Response.json())
+      .then((data) => setLaunches(data.data.launchesPast));
+  }, []);
+
   return (
     <div>
       <h1>SpaceX Mission</h1>
+      {launches.map((launch) => (
+        <li key={launch.id}>{launch.mission_name}</li>
+      ))}
     </div>
   );
 }
